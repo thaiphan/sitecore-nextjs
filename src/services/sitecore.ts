3aa2df1837c4b340c21c98946aecfa7d7fe55f89
Thai Phan <thai@outlook.com>
2021-02-17 18:39:19 +1100

    Support Sitecore instances that don't need the sc_site key

--- src/services/sitecore.ts
+++ src/services/sitecore.ts
@@ -1,15 +1,23 @@
-import { dataApi } from "@sitecore-jss/sitecore-jss-react";
-import axios from "axios";
+import { dataApi } from '@sitecore-jss/sitecore-jss-react';
+import axios from 'axios';
 
 export const getRouteData = async (itemPath: string) => {
+  let querystringParams: {
+    sc_apikey: string;
+    sc_site?: string;
+  } = {
+    sc_apikey: process.env.SITECORE_API_KEY,
+  };
+
+  if (process.env.SITECORE_SITE) {
+    querystringParams.sc_site = process.env.SITECORE_SITE;
+  }
+
   return dataApi.fetchRouteData(itemPath, {
     fetcher: axios.get,
     layoutServiceConfig: {
       serviceUrl: `${process.env.SITECORE_HOST}/sitecore/api/layout/render/jss`,
     },
-    querystringParams: {
-      sc_apikey: process.env.SITECORE_API_KEY,
-      sc_site: process.env.SITECORE_SITE,
-    },
+    querystringParams,
   });
 };
